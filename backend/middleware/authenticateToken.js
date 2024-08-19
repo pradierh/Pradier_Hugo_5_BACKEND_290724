62ee260c0b4fd5400fe6da32bacaa34ef44eb1dd
Hugo Pradier <hpradier@icloud.com>
2024-08-19 17:30:27 +0200

    everythin 's fixed ;)

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secretKey = process.env.SECRET_KEY;
 
@@ -6,19 +6,19 @@ function authMiddleware(req, res, next) {
 	const authHeader = req.headers.authorization;
 
 	if (!authHeader) {
-		return res.status(401).json({ message: "Authentification nécessaire" });
+		return res.status(401).json({ message: 'Authentification nécessaire' });
 	}
-	const token = authHeader.split(" ")[1];
+	const token = authHeader.split(' ')[1];
 	if (!token) {
-		return res.status(401).json({ message: "Token manquant" });
+		return res.status(401).json({ message: 'Token manquant' });
 	}
 
 	try {
-		const decodedToken = jwt.verify(token, "secretKey");
+		const decodedToken = jwt.verify(token, secretKey);
 		req.userId = decodedToken.user_id;
 		next();
 	} catch (error) {
-		return res.status(401).json({ message: "Token invalide ou expiré" });
+		return res.status(401).json({ message: 'Token invalide ou expiré' });
 	}
 }
 
